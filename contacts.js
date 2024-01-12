3285e92b8d55083b49a4a14c7bb2a1a0b967a1ec
Oksana Dziuba <dziuba131303@gmail.com>
2024-01-12 15:27:03 +0400

    writes functions for "list' and "get" cases and implements them

---
+++ contacts.js
@@ -0,0 +1,30 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+console.log(contactsPath);
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find(
+    (contact) => String(contact.id) === String(contactId)
+  );
+  return result;
+
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+module.exports = { listContacts, getContactById };
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { program } from "commander";
+const { program } = require("commander");
+const { listContacts, getContactById } = require("./contacts.js");
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -14,11 +15,39 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      try {
+        const contactsList = await listContacts();
+        console.log(contactsList);
+        return contactsList;
+      } catch (error) {
+        console.log(error);
+      }
       break;
 
     case "get":
+      try {
+        const contactId = "AeHIrLTr6JkxGE6SN-0Rw";
+        // const allContacts = await listContacts();
+        // const ContactIdList = allContacts.map((contact) => contact.id);
+        // const randomIndex = Math.floor(Math.random() * ContactIdList.length);
+        // const randomId = ContactIdList[randomIndex];
+        // console.log(randomId);
+        // const oneBook = await getContactById(randomId);
+        // console.log(oneBook);
+        // return oneBook;
+        // const allContacts = await listContacts();
+        // const ContactIdList = allContacts.map((contact) => contact.id);
+        // const randomIndex = Math.floor(Math.random() * ContactIdList.length);
+        // const contactId = ContactIdList[randomIndex];
+        const oneBook = await getContactById(contactId);
+
+        console.log(oneBook);
+        return oneBook;
+      } catch (error) {
+        console.log(error);
+      }
       // ... id
+
       break;
 
     case "add":
@@ -33,5 +62,7 @@ async function invokeAction({ action, id, name, email, phone }) {
       console.warn("\x1B[31m Unknown action type!");
   }
 }
+
+// invokeAction({ action: "list" });
 
 invokeAction(options);
