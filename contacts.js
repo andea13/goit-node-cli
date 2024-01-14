4e334d0964bbb262bf4f039807b4f67945c38500
Oksana Dziuba <dziuba131303@gmail.com>
2024-01-14 22:41:05 +0400

    adds functions for "add" and "remove" cases and handles them, verifies the code in console

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 console.log(contactsPath);
@@ -11,20 +12,30 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   const contacts = await listContacts();
-  const result = contacts.find(
-    (contact) => String(contact.id) === String(contactId)
-  );
-  return result;
-
-  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
 }
 
 async function removeContact(contactId) {
-  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
 }
 
 async function addContact(name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
+  const contacts = await listContacts();
+  const newContact = { id: nanoid(), name, email, phone };
+  contacts.push(newContact);
+  const updatedJson = JSON.stringify(contacts, null, 2);
+  await fs.writeFile(contactsPath, updatedJson, "utf-8");
+  return newContact;
 }
 
-module.exports = { listContacts, getContactById };
+module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { program } = require("commander");
-const { listContacts, getContactById } = require("./contacts.js");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts.js");
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -15,47 +20,25 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      try {
-        const contactsList = await listContacts();
-        console.log(contactsList);
-        return contactsList;
-      } catch (error) {
-        console.log(error);
-      }
+      const contactsList = await listContacts();
+      console.log(contactsList);
+
       break;
 
     case "get":
-      try {
-        const contactId = "AeHIrLTr6JkxGE6SN-0Rw";
-        // const allContacts = await listContacts();
-        // const ContactIdList = allContacts.map((contact) => contact.id);
-        // const randomIndex = Math.floor(Math.random() * ContactIdList.length);
-        // const randomId = ContactIdList[randomIndex];
-        // console.log(randomId);
-        // const oneBook = await getContactById(randomId);
-        // console.log(oneBook);
-        // return oneBook;
-        // const allContacts = await listContacts();
-        // const ContactIdList = allContacts.map((contact) => contact.id);
-        // const randomIndex = Math.floor(Math.random() * ContactIdList.length);
-        // const contactId = ContactIdList[randomIndex];
-        const oneBook = await getContactById(contactId);
-
-        console.log(oneBook);
-        return oneBook;
-      } catch (error) {
-        console.log(error);
-      }
-      // ... id
+      const oneContact = await getContactById(id);
+      console.log(oneContact);
 
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
       break;
 
     default:
@@ -64,5 +47,13 @@ async function invokeAction({ action, id, name, email, phone }) {
 }
 
 // invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "PlqGej_y5yTcV4L_eY2B1" });
+// invokeAction({
+//   action: "add",
+//   name: "Viktoriia",
+//   email: "vk@gmail.com",
+//   phone: "333-44-55",
+// });
+// invokeAction({ action: "remove", id: "PlqGej_y5yTcV4L_eY2B1" });
 
 invokeAction(options);
